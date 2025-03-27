5f0f47bcc46b7a76029adada64f47c84f454c729
Thiago de Souza Farias <tsf@cin.ufpe.br>
2025-03-27 14:21:42 -0300

    Sfx e Vfx das almondegas

--- Assets/Scripts/Almondega.cs
+++ Assets/Scripts/Almondega.cs
@@ -7,13 +7,27 @@ public class Almondega : MonoBehaviour
     public int almondegaPoints = 10;
     private GameManager _gameManager;
     public Action OnHitSomething;
+    
+    [Header("Particle Effects")]
+    public GameObject hitEffect;
+    
+    [Header("Audio")]
+    public AudioClip hitSound;
+    public AudioClip missSound;
+    public AudioClip throwSound;
+    private AudioSource _audioSource;
 
     private Rigidbody2D _projectileRb;
+    private Collider2D _projectileCollider;
     
     private void Start()
     {
         _gameManager = GameManager.Instance;
         _projectileRb = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>();
+        _projectileCollider = GetComponent<Collider2D>();
+        
+        hitEffect.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,36 +35,58 @@ public class Almondega : MonoBehaviour
         if (other.CompareTag("Panela"))
         {
             Debug.Log("Acertou a panela!");
-            HitPanela();
+            HitPanela(other);
         }
         else if (other.CompareTag("Bounds"))
         {
             Debug.Log("Errou a panela!");
             MissPanela();
         }
-        OnHitSomething?.Invoke();
-        Destroy(this.gameObject);
+        
     }
 
-    private void HitPanela()
+    private void HitPanela(Collider2D other)
     {
+        HitSomething();
         _gameManager.IncrementScore(almondegaPoints);
+        
+        _audioSource.PlayOneShot(hitSound);
+        
+        var effect = Instantiate(hitEffect, other.transform.position, Quaternion.identity);
+        effect.SetActive(true);
+        Destroy(effect, 1f);
     }
 
     private void MissPanela()
     {
+        HitSomething();
         _gameManager.ResetStreak();
+        _audioSource.PlayOneShot(missSound);
+
+    }
+
+    private void HitSomething()
+    {
+        OnHitSomething?.Invoke();
+        Destroy(this.gameObject, 1f);
+        GetComponent<Renderer>().enabled = false;
+        _projectileCollider.enabled = false;
+        _projectileRb.bodyType = RigidbodyType2D.Kinematic;
+        _projectileRb.linearVelocity = Vector2.zero;
+        
     }
 
     public void Launch(Vector2 direction, float force)
     {
         // Ativa o colisor
-        GetComponent<Collider2D>().enabled = true;
+        _projectileCollider.enabled = true;
         
         // Ativa o rastro
         GetComponent<TrailRenderer>().enabled = true;
         
         // aplica força no projectile
         _projectileRb.AddForce(direction * force, ForceMode2D.Impulse);
+        
+        _audioSource.PlayOneShot(throwSound);
     }
 }
